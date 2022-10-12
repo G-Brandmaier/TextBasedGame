59ecf3e8730352a0fbe3dea07df6324a0c531f30
Gabriella <brandmaier.gabriella@gmail.com>
2022-10-12 11:59:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Inlämningsuppgift3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    /*
+    - Spelkaraktär: förflyttar sig, namn samt en lista med föremål som karaktären just nu bär på sig
+    - Platser: förflytta sig mellan, utgångar en eller flera, endpoint
+    - Utgångar: öppen eller låst, gå att öppna med nyckel
+    - Föremål: ta upp, släppa, öppna, använda samt använda på andra föremål
+    - Kommandon: navigera mellan rum, öppna dörrar, använda föremål på andra föremål (byta ut om behövs), 
+    säga till om datorn ej förstår, ta upp eller släppa föremål, rumsbeskrivaning eller inspektera föremål,
+
+
+    Klasser: spelaren, platser, föremål, spelet
+
+     */
+}
